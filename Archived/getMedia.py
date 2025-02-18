edd41902b665d1ac37f277da8a26fa86cf2f183e
afkarxyz <mzamzamafkarhadiq@gmail.com>
2025-02-18 12:12:37 +0700

    Update getMedia.py

--- Archived/getMedia.py
+++ Archived/getMedia.py
@@ -1,12 +1,25 @@
-from gallery_dl import job, config
 import os
+import sys
+from gallery_dl import job, config, extractor
+
+gallery_dl_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, gallery_dl_path)
 
 def get_media(url):
     config.set((), "directory", ["{username}"])
     current_dir = os.path.dirname(os.path.abspath(__file__))
     config.set((), "base-directory", current_dir)
     config.set((), "filename", "{username}_{date}_{num}.{extension}")
-    return job.DownloadJob(url).run()
+    
+    config.set(("extractor", "instagram"), "cookies", {
+        "sessionid": ""
+    })
+    
+    extr = extractor.find(url)
+    if extr is None:
+        raise ValueError(f"No extractor found for URL: {url}")
+    
+    return job.DownloadJob(extr).run()
 
 if __name__ == "__main__":
     url = "https://www.instagram.com/takomayuyi/posts"
